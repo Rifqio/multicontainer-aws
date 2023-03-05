9ae908b0d34a7fd913c6c54ef6f1a3c92e8abe32
Rifqi Oktario <oktariorifqi@gmail.com>
2023-03-05 12:28:38 +0700

    change username db

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "mysql",
-  username: process.env.DATABASE_USERNAME,
+  username: process.env.DATABASE_USER,
   host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_DB,
   password: process.env.DATABASE_PASSWORD,
